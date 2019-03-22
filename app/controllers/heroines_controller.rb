b39f9eff840b78788f3adf538687e108097f6abe
merestrickland <mere.strickland@gmail.com>
2019-03-22 11:12:48 -0400

    werk werk werk werk werk

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,32 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:id, :name, :super_name, :power_id)
+  end
+
+
+
 end
