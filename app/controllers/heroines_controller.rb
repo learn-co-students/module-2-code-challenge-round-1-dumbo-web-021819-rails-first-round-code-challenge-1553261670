a00405a6420394e909eddef192cca6e276f15ad7
Simon Mei <smei8800@gmail.com>
2019-03-22 10:44:40 -0400

    finished deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,44 @@
 class HeroinesController < ApplicationController
+  before_action :get_heroine, only: [:show]
+  
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  # def search
+  #   power = Power.find_by(name: params[:q].downcase)
+  #   byebug
+  #   if power.nil?
+  #     redirect_to heroines_path
+  #   else
+  #     @heroines = Heroine.where(power_id: power.id)
+  #     byebug
+  #   end
+  # end
+
+  private
+
+  def get_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,15 @@
 class PowersController < ApplicationController
+  before_action :get_power, only: [:show]
+  
   def index
     @powers = Power.all
+  end
+
+  def show; end
+
+  private
+
+  def get_power
+    @power = Power.find(params[:id])
   end
 end
