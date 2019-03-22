2361bdbc484af004c4a4e949b660070b5d50cda2
WSINTRA <will.simpson85@gmail.com>
2019-03-22 11:15:42 -0400

    werk werk werk

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,60 @@
 class HeroinesController < ApplicationController
+  before_action :find_by_params, only:[:show, :edit, :update]
+
   def index
-    @heroines = Heroine.all
+ 
+    if params["/search"]
+    binding.pry
+    params["/search"][:power]
+    else
+    	@heroines = Heroine.all
+    end
+
   end
+
+  def show
+ 
+  end
+
+  def edit
+
+  end
+
+  def new
+  	@heroine = Heroine.new
+  end
+
+  def search
+  	binding.pry
+  end
+
+  def create
+
+  	@heroine = Heroine.create(safe_params)
+  	if @heroine.validate
+  		redirect_to heroines_path
+  	else
+  		render :new
+    end
+  	
+  end
+
+
+
+
+private
+
+def find_by_params
+	@heroine = Heroine.find(params[:id])
 end
+
+def safe_params
+	binding.pry
+	params.require(:heroine).permit(:name, :super_name, :power_id)
+	
+end
+
+
+end
+
+
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,29 @@
 class PowersController < ApplicationController
+  before_action :find_by_params, only:[:show, :edit, :update]
+
+
   def index
     @powers = Power.all
   end
+
+    def show
+  	
+  end
+
+  def edit
+  	binding.pry
+  end
+
+  def create
+  	binding.pry
+  end
+
+
+private
+
+def find_by_params
+	@power = Power.find(params[:id])
+end
+
+
 end
