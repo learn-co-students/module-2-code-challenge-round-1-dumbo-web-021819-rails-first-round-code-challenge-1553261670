2c0793c0252094cbd1685472ce6d092fd6f51d41
1-8192 <allegranzia@gmail.com>
2019-03-22 10:43:14 -0400

    Finished base deliverables

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,22 @@
 class PowersController < ApplicationController
+
+  before_action :get_power, only: :show
+
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+
+  private
+
+  def get_power
+    @power = Power.find(params[:id])
+  end
+
+  def get_params
+    params.require(:power).permit(:name, :description)
   end
 end
