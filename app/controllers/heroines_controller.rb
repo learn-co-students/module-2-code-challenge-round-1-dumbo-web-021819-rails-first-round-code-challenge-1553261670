7e96c07a38dd4e1096a3fbfae629635b6dd583b3
sadiebennett1 <sadiebenn1@gmail.com>
2019-03-22 11:13:08 -0400

    Werk done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,26 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @powers = Power.all
+    @heroines = Heroine.search(params[:search])
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(params.require(:heroine).permit(:name, :super_name, :power_id))
+    if @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+
+
 end
