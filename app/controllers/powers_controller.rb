26a0f14b5b378d92a2199395ce90a89465247625
github username <github email address>
2019-03-22 11:13:01 -0400

    Done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,17 @@
 class PowersController < ApplicationController
+  
   def index
     @powers = Power.all
   end
+
+  def show 
+    get_power
+  end
+
+  private 
+
+  def get_power
+    @power = Power.find(params[:id])
+  end
+
 end
